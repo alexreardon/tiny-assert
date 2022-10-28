9822581dd025b1156349fd0799207f59cc103b4b
Alex Reardon <areardon@atlassian.com>
2022-10-28 13:55:59 +1100

    adding empty test file

---
+++ test/functionality.spec.ts
@@ -0,0 +1,12 @@
+import { assert } from '../src/assert.js';
+
+// it('should not throw if condition is truthy', () => {
+//   const truthy: unknown[] = [1, -1, true, {}, [], Symbol(), 'hi'];
+//   truthy.forEach((value: unknown) => expect(() => assert(value)).not.toThrow());
+// });
+
+// it('should throw if the condition is falsy', () => {
+//   // https://github.com/getify/You-Dont-Know-JS/blob/master/types%20%26%20grammar/ch4.md#falsy-values
+//   const falsy: unknown[] = [undefined, null, false, +0, -0, NaN, ''];
+//   falsy.forEach((value: unknown) => expect(() => assert(value)).toThrow());
+// });
